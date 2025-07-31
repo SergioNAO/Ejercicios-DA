6722f64f91382d34fa5148ea2e8b7afe10f3e83e
Sergio Ortiz <65801936+SergioNAO@users.noreply.github.com>
2025-07-31 12:31:19 -0600

    Add files via upload

---
+++ Codigo C#/1.1/ConcatenacionArreglos/ConcatenacionArreglos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    public static string ConcatenaArreglo(object[] px)
+    {
+        return string.Join(",", px);
+    }
+
+    static void Main()
+    {
+        object[] x = { 100, 200, 300, "Hola", 4.2, "Mundo"};
+        string resultado = ConcatenaArreglo(x);
+        Console.WriteLine(resultado); // Salida: 100,200,300
+    }
+}
+
---
+++ Codigo C#/1.3/NombreMes/NombreMes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    public static string Mes(DateTime f)
+    {
+        string[] meses = {
+            "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        return meses[f.Month - 1];
+    }
+
+    static void Main()
+    {
+        DateTime fecha1 = Convert.ToDateTime("24/04/2001");
+        DateTime fecha2 = Convert.ToDateTime("16/08/2001");
+
+        Console.WriteLine(Mes(fecha1)); 
+        Console.WriteLine(Mes(fecha2)); 
+    }
+}
