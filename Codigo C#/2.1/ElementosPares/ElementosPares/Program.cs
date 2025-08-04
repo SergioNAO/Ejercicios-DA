d655ad3ce7d0a3fdebc93f5e444987bc5baf853a
Sergio Ortiz <65801936+SergioNAO@users.noreply.github.com>
2025-08-03 22:43:11 -0600

    Add files via upload

---
+++ Codigo C#/2.1/ElementosPares/ElementosPares/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static int[] ElementosPares(int[] px){
+        List<int> resultado = new List<int>();
+
+        foreach (int num in px){
+            if (num % 2 == 0){
+                resultado.Add(num);
+            }
+        }
+
+        return resultado.ToArray();
+    }
+
+    static void Main(){
+        int[] x = { 1, 2, 3, 10, 22, 33 };
+        int[] res = ElementosPares(x);
+
+        Console.WriteLine("Elementos pares:");
+
+        foreach (int num in res){
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Codigo C#/2.2/OrdenarArreglo/OrdenarArreglo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program{
+    public static int[] OrdenaArreglo(int[] px){
+        Array.Sort(px);
+        
+        return px;
+    }
+
+    static void Main(){
+        int[] x = { 9, 8, 1, 7, 4, 3 };
+        int[] resultado = OrdenaArreglo(x);
+
+        Console.WriteLine("Arreglo ordenado:");
+        foreach (int num in resultado){
+            Console.WriteLine(num);
+        }
+    }
+}
